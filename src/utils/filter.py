ffd2e6d2842228ad1129840b8b30b4b31ed86cf9
Giulio Zabotto <giulio.zabotto@gmail.com>
2022-11-15 09:52:40 +0100

    extreme value theory model

--- src/utils/filter.py
+++ src/utils/filter.py
@@ -6,6 +6,7 @@ import csv
 
 FNAME = "contracts.csv"
 INPUTDIR = "data"
+K = 3
 
 
 def remove_boxcox_exceptions(df: pd.DataFrame, entity: str) -> pd.DataFrame:
@@ -35,9 +36,9 @@ def boxcox(s: pd.Series) -> np.array:
     return stats.boxcox(s)[0]
 
 
-def stdconfint(df: pd.DataFrame, alpha=.05) -> pd.DataFrame:
+def stdconfint(df: pd.DataFrame, entity: str, alpha=.05) -> pd.DataFrame:
     n = len(df)
-    std = np.std(df["amount"], ddof=1)
+    std = np.std(df[ent+"_amount"], ddof=1)
     upper = np.sqrt((n - 1) / stats.chi2.ppf(alpha / 2, n - 1)) * std
     lower = np.sqrt((n - 1) / stats.chi2.ppf((1 - alpha) / 2, n - 1)) * std
     return round(upper - lower, 4)
@@ -48,78 +49,53 @@ if __name__ == "__main__":
     df["start_date"] = pd.to_datetime(df["start_date"])
 
     # BE
-    entity = "id_be"
-    ent = entity.strip("id_")
+    for entity in ["id_be", "id_pa"]:
+        ent = entity.strip("id_")
 
-    # filter out the entities causing the boxcox to fail
-    df = remove_boxcox_exceptions(df, entity)
+        # filter out the entities causing the boxcox to fail
+        df = remove_boxcox_exceptions(df, entity)
 
-    # trasnform the amount feature by IQR scaling and boxcox
-    df[ent+"_amount"] = df["amount"].transform(IQRscale)
-    df[ent+"_amount"] = df.groupby(entity)[ent+"_amount"].transform(boxcox)
+        # trasnform the amount feature by IQR scaling and boxcox
+        df[ent+"_amount"] = df["amount"].transform(IQRscale)
+        df[ent+"_amount"] = df.groupby(entity)[ent+"_amount"].transform(boxcox)
 
-    # compute 3 * standard deviations
-    threestd = df.groupby(entity)[ent+"_amount"].std().rename(ent + "_3std")
-    threestd = 3 * threestd
-    df = df.join(threestd, on=entity)
+        # compute 3 * standard deviations
+        kstd = df.groupby(entity)[ent+"_amount"].std().rename(ent+"_3std")
+        kstd = K * kstd
+        df = df.join(kstd, on=entity)
 
-    # compute the std 95 symmetric confidence interval
-    confint = df.groupby(entity).apply(stdconfint)
-    df = df.join(confint.rename(ent+"_stdconfint"), on=entity)
+        # compute the std 95 symmetric confidence interval
+        confint = df.groupby(entity).apply(lambda x: stdconfint(x, ent), )
+        df = df.join(confint.rename(ent+"_stdconfint"), on=entity)
 
-    # compute the mean
-    mean = df.groupby(entity)[ent+"_amount"].mean()
-    df = df.join(mean.rename(ent+"_mean"), on=entity)
+        # compute the mean
+        mean = df.groupby(entity)[ent+"_amount"].mean()
+        df = df.join(mean.rename(ent+"_mean"), on=entity)
 
-    # Chebishev inequlity
-    mask = np.abs(df[ent+"_amount"] - df[ent + "_mean"]) > df[ent+"_3std"]
-    df[ent + "_probOut"] = mask
-
-    # PA
-    entity = "id_pa"
-    ent = entity.strip("id_")
-
-    # filter out the entities causing the boxcox to fail
-    df = remove_boxcox_exceptions(df, entity)
-
-    # trasnform the amount feature by IQR scaling and boxcox
-    df["amount"] = df["amount"].transform(IQRscale)
-    df["amount"] = df.groupby(entity)["amount"].transform(boxcox)
-
-    # compute 3 * standard deviations
-    threestd = df.groupby(entity)["amount"].std().rename(ent + "_3std")
-    threestd = 3 * threestd
-    df = df.join(threestd, on=entity)
-
-    # compute the std 95 symmetric confidence interval
-    confint = df.groupby(entity).apply(stdconfint)
-    df = df.join(confint.rename(ent+"_stdconfint"), on=entity)
-
-    # compute the mean
-    mean = df.groupby(entity)["amount"].mean()
-    df = df.join(mean.rename(ent+"_mean"), on=entity)
-
-    # Chebishev inequlity
-    mask = np.abs(df["amount"] - df[ent + "_mean"]) > df[ent+"_3std"]
-    df[ent + "_probOut"] = mask
+        # Chebishev inequlity
+        mask = np.abs(df[ent+"_amount"] - df[ent + "_mean"]) > df[ent+"_3std"]
+        df[ent + "_probOut"] = mask
 
     # DURATION
     # replace null duration with 1
     df = df.replace({"duration": 0}, 1)
     # IQR scaling
-    df["duration"] = df["duration"].transform(IQRscale)
+    df["duration_scaled"] = df["duration"].transform(IQRscale)
     # boxcox
-    df["duration"] = boxcox(df["duration"])
+    df["duration_scaled"] = boxcox(df["duration_scaled"])
     # compute mean and std
-    threestd = 3 * df["duration"].std()
-    mean = df["duration"].mean()
+    kstd = K * df["duration_scaled"].std()
+    mean = df["duration_scaled"].mean()
     # Chebishev inquality
-    mask = np.abs(df["duration"] - mean) > threestd
-    df["duration_probOut"] = mask
-
+    mask = np.abs(df["duration_scaled"] - mean) > kstd
+    df["duration_probOut"] = mask   
+    print(sum(df["duration_probOut"]))
+    print(sum(df["be_probOut"]))
+    print(sum(df["pa_probOut"]))
+    print(sum(df.shape))
     # sample dataset
     subset = df.groupby(["id_award_procedure", df.start_date.dt.year],
-                        group_keys=False).apply(lambda x: x.sample(50,
+                        group_keys=False).apply(lambda x: x.sample(frac=1/300,
                                                 random_state=42))
 
     # save as csv
@@ -127,7 +103,7 @@ if __name__ == "__main__":
         "id_lotto", "amount", "duration", "start_date", "id_award_procedure",
         "id_pa", "uber_forma_giuridica", "id_be", "object",
         "be_med_ann_revenue", "pa_med_ann_expenditure", "be_probOut",
-        "pa_probOut", "duration_probOut"
+        "be_stdconfint", "pa_probOut", "pa_stdconfint", "duration_probOut"
         ]
     fname = os.path.join(INPUTDIR, "subset.csv")
     subset[features_to_csv].to_csv(fname, index_label=False, index=False,
