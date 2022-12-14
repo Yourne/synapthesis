5646e3a54ef9e19a3b5b0682d1716c3ce07da7ae
Giulio Zabotto <giulio.zabotto@gmail.com>
2022-12-14 16:08:23 +0100

    histogram plots finished

--- src/utils/feathist.py
+++ src/utils/feathist.py
@@ -10,7 +10,6 @@ data = pd.read_csv(os.path.join(INPUTDIR, "procset.csv"))
 labels = ["framework ass.", "direct ass.", "negotiated proc.", "open proc."]
 
 # "amount","duration","pa_med_ann_expenditure","be_med_ann_revenue"
-var = "amount"
 
 
 def durationhist() -> None:
@@ -24,76 +23,99 @@ def durationhist() -> None:
     fig.supylabel("probability")
     plt.show()
 
-# AMOUNT
+# DURATION
+# var = "duration"
+# for proc, proc_id in zip(["framework", "direct", "negotiated", "open"],
+#                          [26, 23, 4, 1]):
+#     plt.figure()
+#     X = data[data["id_award_procedure"] == proc_id][var]
+#     plt.hist(
+#         X, bins="fd", range=(0, 4000), density=True, log=True)
+#     # ticks = list(x * 1e8 for x in range(0, 7))
+#     # plt.xticks(ticks, [f"{str(int(x/1e6))}M" for x in ticks])
+#     plt.xlabel("duration days")
+#     plt.ylabel("probability")
+#     plt.savefig("histfigures/" + var + "_" + proc + ".png")
 
+# # AMOUNT
 # proc = "framework"
 # X = data[data["id_award_procedure"] == 26]["amount"]
-# maxx = 1e7
+# plt.figure()
 # plt.hist(
-#     X, bins="fd", range=(0, maxx), density=True, log=True)
+#     X, bins="fd", range=(0, 1e7), density=True, log=True)
 # ticks = list(x * 1e6 for x in range(0, 11, 2))
-# plt.xticks(ticks, [f"{str(int(x/1e6))}M" for x in ticks])
-# plt.xlabel("euro")
+# plt.xticks(ticks, [f"{str(int(x/1e6))}M€" for x in ticks])
+# plt.xlabel("lot amount")
 # plt.ylabel("probability")
 # plt.savefig("histfigures/amount_" + proc +".png")
+
 
 # proc = "direct"
 # X = data[data["id_award_procedure"] == 23]["amount"]
-# maxx = 3e5
+# # print((X > 3e5).sum()) # 130
+# # print((X > 1e7).sum()) # 3
+# # print((X > 1e6).sum())  # 34
+# plt.figure()
 # plt.hist(
-#     X, bins="fd", range=(0, maxx), density=True, log=True)
+#     X, bins="fd", range=(0, 3e5), density=True, log=True)
 # ticks = list(x * 1e3 for x in range(0, 301, 50))
-# plt.xticks(ticks, [f"{str(int(x / 1e3))}K" for x in ticks])
-# plt.xlabel("euro")
+# plt.xticks(ticks, [f"{str(int(x / 1e3))}K€" for x in ticks])
+# plt.xlabel("lot amount")
 # plt.ylabel("probability")
-# plt.savefig("histfigures/amount_" + proc +".png")
+# plt.savefig("histfigures/amount_" + proc + ".png")
 
 # proc = "negotiated"
 # X = data[data["id_award_procedure"] == 4]["amount"]
 # maxx = 4e6
+# plt.figure()
 # plt.hist(
 #     X, bins="fd", range=(0, maxx), density=True, log=True)
 # ticks = list(x * 1e6 for x in range(0, 5))
-# plt.xticks(ticks, [f"{str(int(x/1e6))}M" for x in ticks])
-# plt.xlabel("euro")
+# plt.xticks(ticks, [f"{str(int(x/1e6))}M€" for x in ticks])
+# plt.xlabel("lot amount")
 # plt.ylabel("probability")
 # plt.savefig("histfigures/amount_" + proc +".png")
-# plt.show()
+
 
 # proc = "open"
 # X = data[data["id_award_procedure"] == 1]["amount"]
 # maxx = 1e7
+# plt.figure()
 # plt.hist(
 #     X, bins="fd", range=(0, maxx), log=True, density=True)
 # ticks = list(x * 1e6 for x in range(0, 11, 2))
-# plt.xticks(ticks, [f"{str(x/1e6)}M" for x in ticks])
-# plt.xlabel("euro")
+# plt.xticks(ticks, [f"{str(x/1e6)}M€" for x in ticks])
+# plt.xlabel("lot amount")
 # plt.ylabel("probability")
 # plt.savefig("histfigures/amount_" + proc +".png")
 
 
 # BE MEDIAN REV
 # var = "be_med_ann_revenue"
-# proc = "framework"
-# X = data[data["id_award_procedure"] == 26][var]
-# plt.hist(
-#     X, bins="fd", density=True, log=True)
-# ticks = list(x * 1e7 for x in range(0, 13, 2))
-# plt.xticks(ticks, [f"{str(int(x/1e6))}M" for x in ticks])
-# plt.xlabel("euro")
-# plt.ylabel("probability")
-# plt.savefig("histfigures/" + var + "_" + proc + ".png")
-# plt.show()
+# X = data[data["id_award_procedure"] == 23][var]
+# print((X > 3e7).sum(), X.shape) # 1179, 280842
+# for proc, proc_id in zip(["framework", "direct", "negotiated", "open"],
+#                          [26, 23, 4, 1]):
+#     X = data[data["id_award_procedure"] == proc_id][var]
+#     plt.figure()
+#     plt.hist(
+#         X, bins="fd", density=True, log=True)
+#     ticks = list(x * 1e7 for x in range(0, 13, 2))
+#     plt.xticks(ticks, [f"{str(int(x/1e6))}M€" for x in ticks])
+#     plt.xlabel("BE median annual revenue")
+#     plt.ylabel("probability")
+#     plt.savefig("histfigures/" + var + "_" + proc + ".png")
 
-var = "be_med_ann_revenue"
-proc = "direct"
-X = data[data["id_award_procedure"] == 23][var]
-plt.hist(
-    X, bins="fd",  range=(0, 4e7), density=True, log=True)
-ticks = list(x * 1e6 for x in range(0, 41, 5))
-plt.xticks(ticks, [f"{str(int(x/1e6))}M" for x in ticks])
-plt.xlabel("euro")
-plt.ylabel("probability")
-plt.savefig("histfigures/" + var + "_" + proc + ".png")
-plt.show()
-
+# PA MEDIAN ANNUAL EXPENDITURE
+# var = "pa_med_ann_expenditure"
+# for proc, proc_id in zip(["framework", "direct", "negotiated", "open"],
+#                          [26, 23, 4, 1]):
+#     plt.figure()
+#     X = data[data["id_award_procedure"] == proc_id][var]
+#     plt.hist(
+#         X, bins="fd", density=True, log=True)
+#     ticks = list(x * 1e8 for x in range(0, 7))
+#     plt.xticks(ticks, [f"{str(int(x/1e6))}M€" for x in ticks])
+#     plt.xlabel("PA median annual expenditure")
+#     plt.ylabel("probability")
+#     plt.savefig("histfigures/" + var + "_" + proc + ".png")
